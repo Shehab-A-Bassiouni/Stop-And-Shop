b646b6b2113ef96678549ec9910e61fd1a8fe021
Shehab-A-Bassiouni <swe.shehab.a.bassiouni@gmail.com>
2024-05-17 09:09:44 +0300

    Add Context

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
